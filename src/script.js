b950bf8ddb478f33e40089b1d29f0cac0738eede
Anja Helbig <140140377+Unimaus@users.noreply.github.com>
2025-03-01 07:03:56 +0100

    improved link behaviour

--- src/script.js
+++ src/script.js
@@ -1,9 +1,9 @@
 function linkWorldClock() {
-  window.location.href = "https://lovely-malabi-ad6eae.netlify.app/";
+  window.open("https://lovely-malabi-ad6eae.netlify.app/", "_blank");
 }
 
 function linkWeatherApp() {
-  window.location.href = "https://famous-moxie-cc2d96.netlify.app/";
+  window.open("https://famous-moxie-cc2d96.netlify.app/", "_blank");
 }
 
 let wcButton = document.querySelector("#buttonWC");
